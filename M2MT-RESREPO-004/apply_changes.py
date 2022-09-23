da43fbffea8fa43e2b2cc6186e22141acb19b0f3
branko <50580893+branko4@users.noreply.github.com>
2022-09-23 14:10:25 +0200

    Added change of position, location bufferstop

--- M2MT-RESREPO-004/apply_changes.py
+++ M2MT-RESREPO-004/apply_changes.py
@@ -1,4 +1,5 @@
 import time
+import uuid
 
 def handlePassageRef(line):
     BEFORE_PASSAGE_REF = 0
@@ -74,11 +75,20 @@ def handleBufferstop(line):
     newLine = ""
     for char in line:
         if (char == '>' or char == '/'):
-            newLine += 'bufferstopType="fixated"'
-        newLine += char
-    # TODO then add add position reference
-    return f"{newLine}"
+            newLine += ' bufferstopType="fixated"'
+            id = uuid.uuid1()
+            newLine += f' stopsVehicleAt="{id}" '
 
+        newLine += char
+    return f"{newLine}", id
+
+READING_BUFFERSTOP_STATE = 0
+READING_FILE_STATE = 1
+
+bufferstopLocations = []
+index = 0
+
+state = READING_FILE_STATE
 
 try:
     # open XSD file  #, encoding="utf-8")
@@ -101,45 +111,83 @@ try:
             ## wordData = line.split(' ')
             ## for word in wordData:
             # first change jumper and passagerefs
-            if ("<Jumper " in line):
-                newLine = handlePassageRef(line)
-                # print(newLine)
-                newData += f"{newLine}\n"
-                continue
+            # if ("<Jumper " in line):
+            #     newLine = handlePassageRef(line)
+            #     # print(newLine)
+            #     newData += f"{newLine}\n"
+            #     continue
 
             if ("<BufferStop" in line):
                 # TODO then add bufferstop type
                 # TODO then add add position reference
-                newLine = handleBufferstop(line)
+                newLine, id = handleBufferstop(line)
                 newData += f"{newLine}\n"
+                state = READING_BUFFERSTOP_STATE
+                bufferstopLocations.append([id])
                 # print(newLine)
                 continue
 
-            if ("<Metadata" in line):
-                lines = line.split('/')
-                newData += f'{lines[0]} isInService="Unknown" /{lines[1]}\n'
-                continue
-            if ("<Passage " in line):
-                lines = line.split('>')
-                newData += f'{lines[0]} passageSpeed="40" >\n'
-                continue
-            if ("<KCrossing" in line):
-                lines = line.split('>')
-                newData += f'{lines[0]} isMovable="Unknown" operatingType="Unknown" >\n'
-                continue
-            if ("<SwitchBlades" in line):
-                lines = line.split('>')
-                newData += f'{lines[0]} hasSwitchChecker="Unknown" operatingType="Unknown" >\n'
-                continue
-            if ("<SwitchMechanism" in line):
-                newData += line.replace('hasSwitchChecker="Unknown"', "").replace('hasSwitchChecker="True"', "").replace('hasSwitchChecker="False"', "")
-                newData += '\n'
-                continue
-            if ("<SingleSwitch" in line):
-                newData += line.replace('isSymmetric="False"', "").replace('isSymmetric="True"',"").replace('isSymmetric="Unknown"',"").replace('divergingSpeed="40"',"").replace('divergingSpeed="50"',"").replace('divergingSpeed="60"',"").replace('divergingSpeed="80"',"")
-                newData += '\n<DivergingPassageRefs />\n<Passage sideTag="Unknown" passageSpeed="40" puic="d065aa4f-4390-4201-93a8-0ba831fd95b3" > <MetaData /> <Location /> </Passage>'
-                newData += '\n'
-                continue
+            if (state == READING_BUFFERSTOP_STATE):
+                if ("<GeographicLocation" in line):
+                    lines = line.split('<')
+                    bufferstopLocations[index].append(f"<{lines[1]}")
+                    continue
+                if ("<gml:Point" in line):
+                    lines = line.split('<')
+                    bufferstopLocations[index].append(f"\t<{lines[1]}")
+                    continue
+                if ("<gml:coordinates>" in line):
+                    lines = line.split('<gml:coordinates>')[1]#.split('</gml:coordinates>')
+                    bufferstopLocations[index].append(f"\t\t<gml:coordinates>{lines[0]}")
+                    index+=1
+                    state = READING_FILE_STATE
+                    continue
+
+            if ("</Remarks>" in line):
+                newData += f"{line}\n"
+                prefix = line.split('<')[0]
+                newData += f"{prefix}<StopLocations>\n{prefix}\n"
+                for bufferLocation in bufferstopLocations:
+                    newData += f'{prefix}\t\t<StopLocation ID="{bufferLocation[0]}">\n'
+                    newData += f'{prefix}\t\t\t{bufferLocation[1]}\n'
+                    newData += f'{prefix}\t\t\t\t{bufferLocation[2]}\n'
+                    newData += f'{prefix}\t\t\t\t\t{bufferLocation[3]}\n'
+                    newData += f'{prefix}\t\t\t\t</gml:Point>\n'
+                    newData += f'{prefix}\t\t\t</GeographicLocation>\n'
+                    newData += f'{prefix}\t\t</StopLocation>\n'
+                bufferstopLocations = []
+                index=0
+
+
+#     lines = line.split('
+
+
+            # if ("<Metadata" in line):
+            #     lines = line.split('
+            #   \t  lines = line.s2lit('/')
+            #     newData += f'{lines[0]} isInService="Unknown" /{lines[1]}\n'
+            #     continue
+            # if ("<Passage " in line):
+            #     lines = line.split('>')
+            #     newData += f'{lines[0]} passageSpeed="40" >\n'
+            #     continue
+            # if ("<KCrossing" in line):
+            #     lines = line.split('>')
+            #     newData += f'{lines[0]} isMovable="Unknown" operatingType="Unknown" >\n'
+            #     continue
+            # if ("<SwitchBlades" in line):
+            #     lines = line.split('>')
+            #     newData += f'{lines[0]} hasSwitchChecker="Unknown" operatingType="Unknown" >\n'
+            #     continue
+            # if ("<SwitchMechanism" in line):
+            #     newData += line.replace('hasSwitchChecker="Unknown"', "").replace('hasSwitchChecker="True"', "").replace('hasSwitchChecker="False"', "")
+            #     newData += '\n'
+            #     continue
+            # if ("<SingleSwitch" in line):
+            #     newData += line.replace('isSymmetric="False"', "").replace('isSymmetric="True"',"").replace('isSymmetric="Unknown"',"").replace('divergingSpeed="40"',"").replace('divergingSpeed="50"',"").replace('divergingSpeed="60"',"").replace('divergingSpeed="80"',"")
+            #     newData += '\n<DivergingPassageRefs />\n<Passage sideTag="Unknown" passageSpeed="40" puic="d065aa4f-4390-4201-93a8-0ba831fd95b3" > <MetaData /> <Location /> </Passage>'
+            #     newData += '\n'
+            #     continue
 
 
             # save line when none matches
@@ -148,6 +196,7 @@ try:
 finally:
     f.close()
 
+print(bufferstopLocations)
 # for line in newData.split('\n'):
 #     if ("<BufferStop" in line):
 #         print(line)
